008442e47a242843898c4447baa8ed49625c3f7b
freesgen <jesusant.guerrero@gmail.com>
2020-09-09 13:28:40 -0400

    initial commit

---
+++ resources/js/Pages/Board.vue
@@ -0,0 +1,37 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ board.name }}
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                     <div v-for="stage in board.stages" :key="stage.id">
+                         {{ stage.name }}
+                     </div>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from './../Layouts/AppLayout'
+
+    export default {
+        components: {
+            AppLayout,
+        },
+        props: {
+            board: {
+                type: Object,
+                default() {
+                    return {}
+                }
+            }
+        }
+    }
+</script>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+            <div>
+                <jet-application-logo class="block h-12 w-auto" />
+            </div>
+
+            <div class="mt-8 text-2xl">
+                Welcome to your Jetstream application!
+            </div>
+
+            <div class="mt-6 text-gray-500">
+                Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
+                to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
+                you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
+                ecosystem to be a breath of fresh air. We hope you love it.
+            </div>
+        </div>
+
+        <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+            <div class="p-6" v-for="item in items" :key="item.id">
+                <div class="flex items-center">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
+                    <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://laravel.com/docs">{{ item.name }}</a></div>
+                </div>
+
+                <div class="ml-12">
+                    <div class="mt-2 text-sm text-gray-500">
+                        {{ item.description }}
+                    </div>
+
+                    <inertia-link :href="item.link">
+                        <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
+                                <div>Explore {{ item.name }}</div>
+
+                                <div class="ml-1 text-indigo-500">
+                                    <svg viewBox="0 0 20 20" fill="currentColor" class="arrow-right w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </div>
+                        </div>
+                    </inertia-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import JetApplicationLogo from './../Jetstream/ApplicationLogo'
+
+    export default {
+        components: {
+            JetApplicationLogo,
+        },
+        props: {
+            items: {
+                type: Array,
+                default() {
+                    return []
+                }
+            }
+        }
+    }
+</script>
