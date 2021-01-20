c476a7d37281d313a946e9bce261fa9792cbe165
freesgen <jesusant.guerrero@gmail.com>
2021-01-20 00:45:33 -0400

    Update docker prod

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 
-        $schedule->command("daily:automation")->everyMinute();
+        $schedule->command("daily:automation")->everyMinute()->runInBackground();
     }
 
     /**
