bb15fc66b98408169ac79aca7ca3fa9bfc5a6f16
freesgen <jesusant.guerrero@gmail.com>
2021-01-20 15:19:12 -0400

    Add websocket gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Team;
 use App\Policies\TeamPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,16 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewWebSocketsDashboard', function ($user = null) {
+            return in_array($user->email, [
+                'jesusant.guerrero@gmail.com'
+            ]);
+        });
+
+        Gate::define('viewAdminDashboard', function ($user = null) {
+            return in_array($user->email, [
+                'jesusant.guerrero@gmail.com'
+            ]);
+        });
     }
 }
