ebc345dd50ad2ed9619a405ff14a48ab8c2bd0c5
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-06-24 08:24:04 -0400

    fix: case sensitive files

---
+++ app/Actions/Sunday/CreateTaskFromCalendar.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Actions\Sunday;
+
+use App\Events\AutomationCompleted;
+use App\Libraries\GoogleService;
+use App\Models\Automation;
+use App\Models\Board;
+use App\Models\Item;
+use App\Models\Stage;
+use DateTime;
+use Google_Service_Calendar;
+
+class CreateTaskFromCalendar
+{
+    /**
+     * Validate and create a new team for the given user.
+     *
+     * @param  Automation  $automation
+     * @param  Google_Calendar_Events  $calendarEvents
+     * @return void
+     */
+    public static function create(Automation $automation)
+    {
+
+
+        $client = GoogleService::getClient($automation->integration_id);
+        $service = new Google_Service_Calendar($client);
+        $calendarId = 'primary';
+        $results = $service->events->listEvents($calendarId, [
+            'maxResults' => 10,
+            'orderBy' => 'startTime',
+            'singleEvents' => true,
+            'timeMin' => date('c')
+        ]);
+        $calendarEvents = $results->getItems();
+
+        foreach ($calendarEvents as $event) {
+            $config = json_decode($automation->config);
+            $board = Board::find($automation->board_id);
+            $stage = !empty($config->stage_id) ? Stage::find($config->stage_id) : $board->stages[0];
+            $date = new DateTime($event->start->dateTime);
+            $endDate = new DateTime($event->end->dateTime);
+
+            $item = [
+                'title' => $event->getSummary(),
+                'board_id' => $stage->board_id,
+                'stage_id' => $stage->id,
+                'user_id' => $stage->user_id,
+                'team_id' => $stage->team_id,
+                'resource_id' => $event->getId(),
+                'resource_origin' => 'calendar',
+                "resource_type" => 'event',
+                'fields' => [
+                    ['name' => 'date', 'type'=> 'date', 'value' => $date->format('Y-m-d')],
+                    ['name' => 'time', 'type' => 'time', 'value' => $date->format('H:i')],
+                    ['name' => 'due_date', 'type'=> 'date', 'value' => $endDate->format('Y-m-d')],
+                    ['name' => 'end_time', 'type' => 'time', 'value' => $endDate->format('H:i'), 'hide' => true],
+                    ['name' => 'automation_id', 'value' => $automation->id, 'hide' => true],
+                    ['name' => 'url_id', 'type' => 'url', 'value' => $event->hangoutLink, 'hide' => 1],
+                    ['name' => 'url_subject', 'type'=> 'url', 'value' => $event->htmlLink, 'hide' => 1],
+                ]
+            ];
+
+            Item::createEvent($item, [
+                'resource_id' => $item['resource_id'],
+                "resource_origin" => 'calendar',
+                "stage_id" => $item['stage_id']
+            ]);
+        }
+
+        AutomationCompleted::dispatch($automation);
+    }
+}
---
+++ resources/js/components/board/ItemContainerTask.vue
@@ -0,0 +1,128 @@
+<template>
+<div  class="task-item">
+    <div>
+        <label class="checkbox-label">
+            <span class="font-bold">
+            <inertia-link :href="`/boards/${task.board_id}`">
+                <span class="font-bold">
+                    <i class="fas fa-layer-group mx-2"></i>
+                    {{ task.stage }}
+                </span>
+            </inertia-link>
+        </span>
+        <span>
+            {{ task.title }}
+        </span>
+        </label>
+    </div>
+    <div class="actions-container flex items-center">
+        <input
+            type="checkbox"
+            @change="updateTask(task)"
+            name=""
+            class="checkbox-done"
+            :id="task.id"
+            v-model="task.done"
+        />
+        <el-tooltip class="item" effect="dark" :content="task.priority || 'none'" placement="left">
+            <div class="priority-level inline-block ml-2 mr-4">
+                <div class="priority-level__inner">
+
+                </div>
+            </div>
+        </el-tooltip>
+        <span class="mr-2">
+            {{ durationFromMs }}
+        </span>
+        <button @click="$emit('item-clicked', task)" class="play-button" v-if="!task.commit_date">
+            <i class="fa" :class="[isTracker ? 'fa-pause' : 'fa fa-play']"/>
+        </button>
+
+        <button @click="$emit('item-deleted', task)" class="ml-2 text-gray-300 hover:text-red-400">
+            <i class="fa fa-trash"/>
+        </button>
+    </div>
+</div>
+</template>
+
+<script>
+import tracker from './../timeTracker/tracker';
+import Tracker from "./../timeTracker/tracker";
+
+export default {
+    props: {
+        task: {
+            type: Object,
+            required: true
+        },
+        tracker: {
+            type: Object
+        }
+    },
+
+    data() {
+        return {
+            originalDuration: null
+        }
+    },
+
+    watch: {
+        tracker(tracker) {
+            if (tracker) {
+                this.originalDuration = this.task.duration
+            } else {
+                this.originalDuration = this.task.duration
+            }
+        }
+    },
+
+    created() {
+        this.originalDuration = this.task.duration;
+    },
+
+    computed: {
+        isTracker() {
+            return this.tracker && this.tracker.timeEntry.item_id == this.task.id;
+        },
+        durationFromMs() {
+            const currentDuration = this.isTracker ? this.tracker.duration || 0: 0;
+
+            this.$set(this.task, 'duration', this.originalDuration + currentDuration);
+            return Tracker.durationFromMs(this.task.duration);
+        },
+        priorityText() {
+            const emojis = {
+                "high": "🔥🔥🔥",
+                "medium": "🔥🔥",
+                "low": "🔥"
+            }
+            return this.task && emojis[this.task.priority] || "";
+        }
+    },
+    methods: {
+         updateTask() {
+            this.$emit('update-item', this.task)
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.priority-level {
+    --color: red;
+    border: solid 1px transparent;
+    border-radius: 0.20rem;
+    padding: 2px;
+    transition: all ease .3s;
+
+    &:hover {
+        border: solid 1px var(--color);
+    }
+    &__inner {
+        background-color: var(--color);
+        width: 12px;
+        height: 12px;
+        border-radius: 0.20rem;
+    }
+}
+</style>
