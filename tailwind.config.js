4c2025b7200459088479bc766abc5a9c12d7e903
Jesus Guerrero <jesusant.guerrero@gmail.com>
2022-06-23 23:32:24 -0400

    chore: update tailwindcss & mix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: {
-        mode: 'layers',
-        options: {
-            whitelist: [
-                'bg-red-400',
-                'bg-green-400',
-                'bg-yellow-400',
-                'bg-blue-400',
-                'bg-purple-400',
-                'text-green-400',
-                'text-yellow-400',
-                'text-blue-400',
-                'text-purple-400',
-                'font-bold',
-                'bg-red-300',
-                'bg-green-300',
-                'bg-yellow-300',
-                'bg-blue-300',
-                'bg-purple-300',
-            ]
-        },
-        content: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php', './resources/js/**/*.vue']
-    },
+    content: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php', './resources/js/**/*.vue'],
     theme: {
         extend: {
             fontFamily: {
@@ -31,10 +9,5 @@ module.exports = {
             },
         },
     },
-
-    variants: {
-        opacity: ['responsive', 'hover', 'focus', 'disabled'],
-    },
-
     plugins: [require('@tailwindcss/ui'), require('@tailwindcss/typography')],
 };
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,16 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue()
 .sass('resources/css/app.scss', 'public/css').options({
     postCss: [
         require('postcss-import'),
         require('tailwindcss'),
+        require('autoprefixer'),
     ]
 })
 .webpackConfig(require('./webpack.config'))
+
+
+if (mix.inProduction()) {
+    mix.version();
+}
